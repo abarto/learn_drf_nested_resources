ad9330303836b3c2ad012c1052046f4da81af490
Agustin Barto <abarto@gmail.com>
2015-06-27 20:22:11 -0300

    Create author permission

--- learn_drf_nested_resources/blogposts/views.py
+++ learn_drf_nested_resources/blogposts/views.py
@@ -1,12 +1,15 @@
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Blogpost, Comment
+from .permissions import IsAuthorOrReadOnly
 from .serializers import BlogpostSerializer, CommentSerializer
 
 
 class BlogpostViewSet(ModelViewSet):
     serializer_class = BlogpostSerializer
     queryset = Blogpost.objects.all()
+    permission_classes = (IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
